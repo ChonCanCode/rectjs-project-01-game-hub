b155f0154ca009cda22bb78a080dad0f2255ac28
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-01-01 10:15:02 +0000

    Update ListGroup.tsx

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
   const items = [
@@ -9,14 +10,24 @@ function ListGroup() {
     'Paris'
   ];
 
-//
+
+  
+  const handleClick = (event: MouseEvent) => console.log(event)
+
+
+  
   return (
     <>
         <h1>List</h1>
         {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
-          {items.map((item) => (
-            <li key={item}>{item}</li>
+          {items.map((index, item) => (
+            <li className="list-group-item" 
+            key={index} 
+            onClick={handleClick}
+            >
+              {index}
+              </li>
             ))}
         </ul>
     </>
