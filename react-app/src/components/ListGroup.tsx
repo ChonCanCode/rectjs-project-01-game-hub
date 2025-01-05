6b741026bc8f9c9793424eb3c627f2530feaff31
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-01-05 07:39:46 +0000

    learning props

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,20 +1,16 @@
 import { Fragment, useState } from "react";
 
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  let items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
-
+function ListGroup({ items, heading}: Props) {
  const [selectedIndex, setSelectedindex] = useState(-1);
 
   return (
     <>
-        <h1>List</h1>
+        <h1>{heading}</h1>
         {items.length === 0 && <p>No item found</p>}
         <ul 
         className="list-group">
